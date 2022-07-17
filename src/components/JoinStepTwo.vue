0d9ba5b174ad4befe6579a109a7c518908383e29
MOON <sunpositive7@gmail.com>
2022-07-17 13:49:44 +0900

    우편번호 찾기 기능 추가

--- src/components/JoinStepTwo.vue
+++ src/components/JoinStepTwo.vue
@@ -30,6 +30,26 @@
         >휴대전화 번호 형식이 맞지 않습니다.</span
       >
     </div>
+    <div class="join-form join-form--address">
+      <div class="join-form__title"><label for="inputaddress">주소</label></div>
+      <div class="join-form__input">
+        <button type="button" id="inputaddress" @click="execDaumPostcode()">
+          우편번호 찾기
+        </button>
+        <input
+          type="text"
+          v-model="signup.addressBasic"
+          readonly
+          title="도로명 주소"
+        />
+        <input
+          type="text"
+          v-model="signup.addressDetail"
+          title="상세 주소"
+          placeholder="상세주소 입력"
+        />
+      </div>
+    </div>
   </div>
 </template>
 
@@ -67,6 +87,52 @@ export default {
         this.phoneValidFlag = false;
       }
     },
+    execDaumPostcode() {
+      new window.daum.Postcode({
+        oncomplete: (data) => {
+          // 팝업에서 검색결과 항목을 클릭했을때 실행할 코드를 작성하는 부분.
+
+          // 도로명 주소의 노출 규칙에 따라 주소를 표시한다.
+          // 내려오는 변수가 값이 없는 경우엔 공백('')값을 가지므로, 이를 참고하여 분기 한다.
+          let roadAddr = data.roadAddress; // 도로명 주소 변수
+          let extraRoadAddr = ""; // 도로명 조합형 주소 변수
+
+          // 법정동명이 있을 경우 추가한다. (법정리는 제외)
+          // 법정동의 경우 마지막 문자가 "동/로/가"로 끝난다.
+          if (data.bname !== "" && /[동|로|가]$/g.test(data.bname)) {
+            extraRoadAddr += data.bname;
+          }
+          // 건물명이 있고, 공동주택일 경우 추가한다.
+          if (data.buildingName !== "" && data.apartment === "Y") {
+            extraRoadAddr +=
+              extraRoadAddr !== ""
+                ? ", " + data.buildingName
+                : data.buildingName;
+          }
+          // 표시할 참고항목이 있을 경우, 괄호까지 추가한 최종 문자열을 만든다.
+          if (extraRoadAddr !== "") {
+            extraRoadAddr = " (" + extraRoadAddr + ")";
+          }
+          // 참고항목 문자열이 있을 경우 해당 필드에 넣는다.
+          if (roadAddr !== "") {
+            roadAddr += extraRoadAddr;
+          }
+
+          this.signup.addressBasic = roadAddr;
+        },
+      }).open();
+    },
   },
 };
 </script>
+
+<style lang="scss">
+.join-form.join-form--address {
+  .join-form__input {
+    width: 300px;
+    input {
+      margin-top: 5px;
+    }
+  }
+}
+</style>
